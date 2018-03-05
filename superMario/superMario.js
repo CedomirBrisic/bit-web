a0eaef5cbc3fec845add0990ba7e3e0f33861be2
Cedomir Brisic <36138040+CedomirBrisic@users.noreply.github.com>
2018-03-05 21:18:30 +0100

    it runns :-)

--- superMario/superMario.js
+++ superMario/superMario.js
@@ -5,18 +5,25 @@ var pozadina = document.querySelector(".pozadina");
 document.onkeydown = run;
 document.onkeyup = stop;
 
+var index = 0;
+
+var moving = function () {
+    index -= 20;
+    pozadina.style = "background-position-x: " + index + "px";
+};
 
 function run(x) {
     if (x.keyCode == '39') {
         stopMario.style = "z-index: -1";
         runMario.style = "z-index: 1";
-        pozadina.style = "background-position-x: -20px";
+        setInterval (moving(),500);
     }
 }
 
-function stop(x){
-    if(x.keyCode == "39"){
+function stop(x) {
+    if (x.keyCode == "39") {
         stopMario.style = "z-index: 1";
-            runMario.style = "z-index: -1";  
+        runMario.style = "z-index: -1";
+        clearInterval(moving);
     }
 }
