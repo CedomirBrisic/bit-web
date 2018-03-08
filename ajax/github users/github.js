5b5f05f43e61470420881e06f1cdc6d864171288
Cedomir Brisic <36138040+CedomirBrisic@users.noreply.github.com>
2018-03-08 23:24:13 +0100

    github response v1

---
+++ ajax/github users/github.js
@@ -0,0 +1,23 @@
+$button = $("#searchButton");
+$button.on("click", getUser);
+
+// $searchInput = $searchInputNode.toString();
+
+function getUser() {
+    $searchInput = $("#searchInput").val();
+    var input = String($searchInput);
+    var requestData = `"https://api.github.com/search/users?q=${input}"`
+    
+    var newRequest = new XMLHttpRequest();
+    newRequest.open("GET",requestData);
+    newRequest.send();
+
+    newRequest.onload = function () {
+        var response = newRequest.responseXML;
+        var data = JSON.parse(response.responseText);
+
+        console.log(data);
+    }
+ 
+    console.log(requestData);
+}
